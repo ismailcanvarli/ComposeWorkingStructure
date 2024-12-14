a481bd2183f2ec9ec6d6870764b16547ad33c278
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-14 11:30:48 +0300

    Add opening and closing effects and implement backstage functionality

--- app/src/main/java/com/example/workingstructure/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/workingstructure/ui/screens/DetailScreen.kt
@@ -2,6 +2,8 @@
 
 package com.example.workingstructure.ui.screens
 
+import android.util.Log
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +15,13 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun DetailScreen(message: String) {
+    // Geri tuşuna basıldığında çalışacak kodlar buraya yazılır.
+    // true olursa geri tuşu işlevsiz hale gelir.
+    // false olursa geri tuşu çalışır.
+    BackHandler(false) {
+        Log.e("DetailScreen", "BackHandler")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
--- app/src/main/java/com/example/workingstructure/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/workingstructure/ui/screens/HomeScreen.kt
@@ -2,12 +2,15 @@
 
 package com.example.workingstructure.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -18,6 +21,17 @@ import androidx.navigation.NavController
 @Composable
 fun HomeScreen(navController: NavController) {
     val timer = remember { mutableStateOf(0) }
+    LaunchedEffect(key1 = true) {
+        // Sayfa açıldığında yada geri döndüğünde çalışacak kodlar buraya yazılır.
+        Log.e("HomeScreen", "LaunchedEffect")
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            // Sayfa kapatıldığında çalışacak kodlar buraya yazılır.
+            Log.e("HomeScreen", "LaunchedEffect")
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -33,9 +47,17 @@ fun HomeScreen(navController: NavController) {
         }
         Button(onClick = {
             if (timer.value == 10) {
-                navController.navigate("detailScreen/İsmail")
+                navController.navigate("detailScreen/İsmail") {
+                    popUpTo("homeScreen") {
+                        inclusive = true
+                    }
+                }
             } else {
-                navController.navigate("detailScreen/${timer.value}")
+                navController.navigate("detailScreen/${timer.value}") {
+                    popUpTo("homeScreen") {
+                        inclusive = true
+                    }
+                }
             }
         }) {
             Text(text = "Detail")
